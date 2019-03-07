acfd128335074817418ccbddb8e586723dded9f2
mehremic1 <mehremic.a@gmail.com>
2019-03-07 15:17:12 +0100

    implement method think

--- neural_network.py
+++ neural_network.py
@@ -1,13 +1,20 @@
+# Simple neural network
+
 import math
 import random
 
 class NeuralNetwork():
 
     def __init_(self):
-        pass
+        random.seed(1)
+        self.weights = [random.uniform(-1, 1),
+                        random.uniform(-1,1),
+                        random.uniform(-1,1)]
 
     def think(self, neuron_inputs):
-        pass
+        sum_of_weighted_inputs = self.__sum_of_weighted_inputs(neuron_inputs)
+        neuron_output = self.__sigmoid(sum_of_weighted_inputs)
+        return neuron_output
 
     def train(self):
         pass
@@ -21,6 +28,7 @@ class NeuralNetwork():
     def __sum_of_weighted_inputs(self):
         pass
 
+# Traning set for neural network
 training_set_examples = [{'inputs': [0, 0, 1], 'output': 0},
                          {'inputs': [1, 1, 1], 'output': 1},
                          {'inputs': [1, 0, 1], 'output': 1},
