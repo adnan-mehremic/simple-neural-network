ccefd7befd7fd808004d589f304e5e5ae3091fdb
mehremic1 <mehremic.a@gmail.com>
2019-03-07 14:30:43 +0100

    add all methods

---
+++ neural_network.py
@@ -0,0 +1,28 @@
+import math
+import random
+
+class NeuralNetwork():
+
+    def __init_(self):
+        pass
+
+    def think(self, neuron_inputs):
+        pass
+
+    def train(self):
+        pass
+
+    def __sigmoid(self):
+        pass
+
+    def __sigmoid_gradient(self):
+        pass
+
+    def __sum_of_weighted_inputs(self):
+        pass
+
+training_set_examples = [{'inputs': [0, 0, 1], 'output': 0},
+                         {'inputs': [1, 1, 1], 'output': 1},
+                         {'inputs': [1, 0, 1], 'output': 1},
+                         {'inputs': [0, 1, 1], 'output': 1}]
+
