7d3156b5252842e6814cbd07f6dc9372c9dc3937
mehremic1 <mehremic.a@gmail.com>
2019-03-09 19:22:53 +0100

    add file for simple neural network

--- neural_network.py
+++ neural_network.py
@@ -6,13 +6,13 @@ import random
 
 class NeuralNetwork:
 
-    def __init_(self):
+    def __init__(self):
         random.seed(1)
         self.weights = [random.uniform(-1, 1),
                         random.uniform(-1, 1),
                         random.uniform(-1, 1)]
 
-    def think(self, neuron_inputs):
+    def predict(self, neuron_inputs):
         sum_of_weighted_inputs = self.__sum_of_weighted_inputs(neuron_inputs)
         neuron_output = self.__sigmoid(sum_of_weighted_inputs)
         return neuron_output
@@ -20,18 +20,12 @@ class NeuralNetwork:
     def train(self, training_set_examples, number_of_iterations):
         for iteration in range(number_of_iterations):
             for training_set_example in training_set_examples:
-                predicted_output = self.think(training_set_example["inputs"])
-
+                predicted_output = self.predict(training_set_example["inputs"])
                 error_in_output = training_set_example["output"] - predicted_output
                 for index in range(len(self.weights)):
-
                     neuron_input = training_set_example["inputs"][index]
-
                     adjust_weight = neuron_input * error_in_output * self.__sigmoid_gradient(predicted_output)
-
                     self.weights[index] += adjust_weight
-
-
 
     def __sigmoid(self, sum_of_weighted_inputs):
         return 1/(1 + math.exp(-sum_of_weighted_inputs))
@@ -46,8 +40,20 @@ class NeuralNetwork:
         return sum_of_weighted_inputs
 
 
-# Training set for neural network
-training_set_examples = [{'inputs': [0, 0, 1], 'output': 0},
-                         {'inputs': [1, 1, 1], 'output': 1},
-                         {'inputs': [1, 0, 1], 'output': 1},
-                         {'inputs': [0, 1, 1], 'output': 1}]
+neural_network = NeuralNetwork()
+
+print("Random starting weights: " + str(neural_network.weights))
+
+training_set_examples = [{"inputs": [0, 0, 1], "output": 0},
+                         {"inputs": [1, 1, 1], "output": 1},
+                         {"inputs": [1, 0, 1], "output": 1},
+                         {"inputs": [0, 1, 1], "output": 0}]
+
+neural_network.train(training_set_examples, number_of_iterations=10000)
+
+print("New weights after training: " + str(neural_network.weights))
+
+new_example = [1, 0, 0]
+prediction = neural_network.predict(new_example)
+
+print("Prediction for the new example [1, 0, 0]: " + str(prediction))
